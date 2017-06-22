d369595fb8d309ff175b1bb4c59f0f096d0b6d3e
Don Kingery <kingery1@suddenlink.net>
2017-06-22 13:55:53 -0400

    classes revisited

---
+++ Classes.rb
@@ -0,0 +1,46 @@
+#class Kitty
+#    def meow
+#        puts "meow meow"
+#    end
+#    
+#      def eat
+#        puts "nom nom nom"
+#    end
+#    
+#    def purr
+#        puts "purrrrr"
+#    end
+#end
+#
+#gucci = Kitty.new
+#gucci.meow
+#gucci.eat
+#gucci.purr
+#gucci.meow
+
+class BarnSounds
+    def chicken
+        puts "cluck cluck cluck"
+    end
+    
+    def cow
+        puts "moo moo moo"
+    end
+        
+    def cat 
+        puts "meow meow"
+    end
+    
+    def duck
+        puts "quack quack quack"
+    end
+end
+
+GreenFarm =  BarnSounds.new
+GreenFarm.chicken
+GreenFarm.duck
+GreenFarm.cow
+GreenFarm.cat
+
+    
+    
