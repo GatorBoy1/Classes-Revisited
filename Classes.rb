25ad66db033e60f2c0a19639435683178aa6e3ba
Don Kingery <kingery1@suddenlink.net>
2017-06-24 15:57:42 -0400

    added to classes

--- Classes.rb
+++ Classes.rb
@@ -1,46 +1,46 @@
-#class Kitty
-#    def meow
-#        puts "meow meow"
-#    end
-#    
-#      def eat
-#        puts "nom nom nom"
-#    end
-#    
-#    def purr
-#        puts "purrrrr"
-#    end
-#end
-#
-#gucci = Kitty.new
-#gucci.meow
-#gucci.eat
-#gucci.purr
-#gucci.meow
+class Kitty
+    def meow
+        puts "meow meow"
+    end
+    
+      def eat
+        puts "nom nom nom"
+    end
+    
+    def purr
+        puts "purrrrr"
+    end
+end
 
-#class BarnSounds
-#    def chicken
-#        puts "cluck cluck cluck"
-#    end
-#    
-#    def cow
-#        puts "moo moo moo"
-#    end
-#        
-#    def cat 
-#        puts "meow meow"
-#    end
-#    
-#    def duck
-#        puts "quack quack quack"
-#    end
-#end
-#
-#GreenFarm =  BarnSounds.new
-#GreenFarm.chicken
-#GreenFarm.duck
-#GreenFarm.cow
-#GreenFarm.cat
+gucci = Kitty.new
+gucci.meow
+gucci.eat
+gucci.purr
+gucci.meow
+
+class BarnSounds
+    def chicken
+        puts "cluck cluck cluck"
+    end
+    
+    def cow
+        puts "moo moo moo"
+    end
+        
+    def cat 
+        puts "meow meow"
+    end
+    
+    def duck
+        puts "quack quack quack"
+    end
+end
+
+GreenFarm =  BarnSounds.new
+GreenFarm.chicken
+GreenFarm.duck
+GreenFarm.cow
+GreenFarm.cat
 
 class FavPerson
     def leader
@@ -59,13 +59,13 @@ class FavPerson
         puts "Bruce Lee"
     end
     
-    def constitution
+    def constitutional_author
         puts "Thomas Jefferson"
     end
 end
 
 DonFav = FavPerson.new
-DonFav.constitution
+DonFav.constitutional_author
 DonFav.martial_artist
 DonFav.comedian
 DonFav.actor
